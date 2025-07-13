0a529675efe3334741b13e71121ac95c904e2d78
dhruvesh131199 <dhruveshccc2200@gmail.com>
2025-07-12 20:21:16 -0600

    Initial commit

---
+++ src/test/java/com/example/options/OptionsRecommenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.options;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptionsRecommenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
